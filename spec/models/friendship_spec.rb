7c5f1006a3838001dc7778831cbd5a83b0a836ec
HillaryOkello <hillariouskelly@outlook.com>
2021-02-26 18:50:15 +0300

    small change

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Friendship, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
-  let(:user) { User.new(name: 'Peter', email: 'x@x.com', password: '123456') }
-  let(:friend) { User.new(name: 'John', email: 'y@y.com', password: '123456') }
+  let(:user) { User.new(name: 'Hillary', email: 'hillary@x.com', password: 'abcd') }
+  let(:friend) { User.new(name: 'Kelly', email: 'kelly@y.com', password: 'abcd') }
   let(:friendship) { Friendship.create(user: user, friend: friend) }
 
   it 'creates a friendship correctly' do
