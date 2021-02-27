360b4427e7cde7a26f98817cfbafaebc24da1145
HillaryOkello <hillariouskelly@outlook.com>
2021-02-27 22:06:15 +0300

    add test for like

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+  it 'creates a like correctly' do
+    user = User.new(name: 'Hillary', email: 'hillary@x.com', password: '123456')
+    post = Post.create(content: 'Hello!', user: user)
+    like = Like.create(user: user, post: post)
+    expect(like.valid?).to be true
+  end
 end
